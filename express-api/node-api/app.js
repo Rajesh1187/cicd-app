b0cfcd59084ffcc01f498fc562cac44f17c42269
Rajesh1187 <rajesh.singh@cardekh.com>
2026-05-05 17:18:56 +0530

    Clean CI/CD repo structure

--- express-api/node-api/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("🚀 Node.js CI/CD Running!");
-});
-
-app.listen(3000, () => console.log("Server running"));
